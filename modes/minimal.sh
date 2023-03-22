d51753987559eecd62e09853b58dacb9288f34b0
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-03-22 11:06:32 +1100

    Make it more minimal

--- modes/minimal.sh
+++ modes/minimal.sh
@@ -3,11 +3,13 @@ tmux set -g pane-border-style "fg=colour8"
 tmux set -g pane-active-border-style "fg=colour4"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-current-format "#[fg=colour6] #{s|1|#[fg=colour1]Z|;s|0|●|:window_zoomed_flag}#[fg=colour6] #W "
-tmux setw -g window-status-current-format "#[fg=colour6] #{s|1|#[fg=colour1]Z|;s|0|●|:window_zoomed_flag}#[fg=colour6]─#W"
-tmux setw -g window-status-format "#[fg=colour8] ○ " #W
+tmux setw -g window-status-format "#[fg=colour8] ● " #W
 tmux set -g status-right ' '
 tmux set -g status-left ' '
 tmux set -g status-justify centre
 tmux set -g status-position bottom
 # hide when default
 tmux set -g status-right ' #{?#{==:#S,default},,#S} '
+
+tmux setw -g window-status-format "#[fg=colour8] ○ " #W
+tmux setw -g window-status-current-format "#[fg=colour6] #{s|1|#[fg=colour1]Z|;s|0|●|:window_zoomed_flag}#[fg=colour6] "
--- modes/verbose.sh
+++ modes/verbose.sh
@@ -4,7 +4,7 @@ tmux set -g pane-active-border-style "fg=colour4"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-current-format "#[fg=colour6]  #{s|1|#[fg=colour1]Z|;s|0|●|:window_zoomed_flag}#[fg=colour6] #W"
 tmux setw -g window-status-format "#[fg=colour8]  ● #W"
-tmux set -g status-right '#{?#{==:#S,default},,  ──  #S}'
+tmux set -g status-right '#{?#{==:#S,default},,  #S}'
 tmux set -g status-left ' '
 tmux set -g status-position bottom
-tmux set -g status-justify right
+tmux set -g status-justify centre
