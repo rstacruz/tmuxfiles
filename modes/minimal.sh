2a8578597d3425d40c4e99cfe40dde48c0b161a2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-04-11 08:58:10 +1000

    Update modes/minimal.sh (+2 more changes)

--- modes/minimal.sh
+++ modes/minimal.sh
@@ -8,8 +8,8 @@ tmux set -g status-right ' '
 tmux set -g status-left ' '
 tmux set -g status-justify centre
 tmux set -g status-position bottom
-# hide when default
-tmux set -g status-right ' #{?#{==:#S,default},,#S} '
+tmux set -g status-right '#{?#{==:#S,0},,  #S}'
+# ^ #{?#{==:#S,0}} = if the current session is 0, don't show the session number
 
 tmux setw -g window-status-format "#[fg=colour8] ○ " #W
 tmux setw -g window-status-current-format "#[fg=colour6] #{s|1|#[fg=colour1]Z|;s|0|●|:window_zoomed_flag}#[fg=colour6] "
--- modes/verbose.sh
+++ modes/verbose.sh
@@ -4,7 +4,8 @@ tmux set -g pane-active-border-style "fg=colour4"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-current-format "#[fg=colour6]  #{s|1|#[fg=colour1]Z|;s|0|●|:window_zoomed_flag}#[fg=colour6] #W"
 tmux setw -g window-status-format "#[fg=colour8]  ○ #W"
-tmux set -g status-right '#{?#{==:#S,default},,  #S}'
+tmux set -g status-right '#{?#{==:#S,0},,  #S}'
+# ^ #{?#{==:#S,0}} = if the current session is 0, don't show the session number
 tmux set -g status-left ' '
 tmux set -g status-position bottom
 tmux set -g status-justify centre
