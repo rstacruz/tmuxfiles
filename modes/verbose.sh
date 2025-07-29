83b1c69b2d1bf901bab218f3586de8d369bcd5b3
Rico Sta. Cruz <rstacruz25@users.noreply.github.com>
2025-07-29 13:56:28 +1000

    feat: update theme colours

--- modes/verbose.sh
+++ modes/verbose.sh
@@ -6,7 +6,7 @@ tmux set -g status-style 'fg=colour8,bg=colour234'
 tmux set -g pane-border-style "fg=colour8"
 tmux set -g pane-active-border-style "fg=colour4"
 tmux setw -g window-status-separator '#[fg=colour236]  / '
-tmux setw -g window-status-current-format " #{s|1|#[fg=colour1]$icon_zoom|;s|0|$icon_active|:window_zoomed_flag}#[fg=colour8] #W"
+tmux setw -g window-status-current-format " #{s|1|#[fg=colour1]$icon_zoom|;s|0|$icon_active|:window_zoomed_flag}#[fg=colour3] #W"
 tmux setw -g window-status-format "#[fg=colour8] $icon_inactive #W"
 tmux set -g status-left '#[bg=colour236,fg=colour6]#{?#{==:#S,0},,  #S}  #[default] '
 # ^ #{?#{==:#S,0}} = if the current session is 0, don't show the session number
