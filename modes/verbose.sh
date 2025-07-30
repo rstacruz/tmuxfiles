d592658b256befec7cd25d67f14c901aef3cc582
Rico Sta. Cruz <rstacruz25@users.noreply.github.com>
2025-07-31 09:44:09 +1000

    Update modes/verbose.sh

--- modes/verbose.sh
+++ modes/verbose.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-icon_active="󰇊"
-icon_inactive="󱅊"
+icon_active="━"
+icon_inactive="━" # 󱅊 󰇊
+icon_sep="  │  "
 icon_zoom="󰊓"
 tmux set -g status-style 'fg=colour8,bg=colour234'
 tmux set -g pane-border-style "fg=colour8"
 tmux set -g pane-active-border-style "fg=colour4"
-tmux setw -g window-status-separator '#[fg=colour236]  / '
+tmux setw -g window-status-separator "#[fg=colour236] $icon_sep"
 tmux setw -g window-status-current-format " #{s|1|#[fg=colour1]$icon_zoom|;s|0|$icon_active|:window_zoomed_flag}#[fg=colour3] #W"
 tmux setw -g window-status-format "#[fg=colour8] $icon_inactive #W"
 tmux set -g status-left '#[bg=colour236,fg=colour6]#{?#{==:#S,0},,  #S}  #[default] '
