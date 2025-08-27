f1df888c7d48bd82a8d842c1c340432fdd9006d0
Rico Sta. Cruz <rstacruz25@users.noreply.github.com>
2025-08-27 16:38:41 +1000

    refactor(modes): use colour243 variable for pane and status fg
    - Replace hardcoded 'colour8' with 'colour243' variable for foreground in minimal.sh and verbose.sh
    - Improves consistency and maintainability of colour settings

--- modes/minimal.sh
+++ modes/minimal.sh
@@ -2,11 +2,12 @@
 icon_active=" ━━━ "
 icon_inactive=" ─── "
 icon_zoom="󰊓"
-tmux set -g pane-border-style "fg=colour8"
+colour_fg="colour243"
+tmux set -g pane-border-style "fg=$colour_fg"
 tmux set -g pane-active-border-style "fg=colour4"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-current-format "#[fg=colour6] #{s|1|#[fg=colour1]$icon_zoom|;s|0|$icon_active|:window_zoomed_flag}#[fg=colour6] #W "
-tmux setw -g window-status-format "#[fg=colour8] $icon_active " #W
+tmux setw -g window-status-format "#[fg=$colour_fg] $icon_active " #W
 tmux set -g status-right ' '
 tmux set -g status-left ' '
 tmux set -g status-justify centre
@@ -14,5 +15,5 @@ tmux set -g status-right ''
 tmux set -g status-left '#[bg=colour236,fg=colour6]#{?#{==:#S,0},,  #S}  #[default] '
 # ^ #{?#{==:#S,0}} = if the current session is 0, don't show the session number
 
-tmux setw -g window-status-format "#[fg=colour8] $icon_inactive " #W
+tmux setw -g window-status-format "#[fg=$colour_fg] $icon_inactive " #W
 tmux setw -g window-status-current-format "#[fg=colour6] #{s|1|#[fg=colour1]$icon_zoom|;s|0|$icon_active|:window_zoomed_flag}#[fg=colour6] "
--- modes/verbose.sh
+++ modes/verbose.sh
@@ -3,12 +3,13 @@ icon_active="━"
 icon_inactive="━" # 󱅊 󰇊
 icon_sep="  │  "
 icon_zoom="󰊓"
-tmux set -g status-style 'fg=colour8,bg=colour234'
-tmux set -g pane-border-style "fg=colour8"
+colour_fg="colour243"
+tmux set -g status-style "fg=$colour_fg,bg=colour234"
+tmux set -g pane-border-style "fg=$colour_fg"
 tmux set -g pane-active-border-style "fg=colour4"
 tmux setw -g window-status-separator "#[fg=colour236] $icon_sep"
 tmux setw -g window-status-current-format " #{s|1|#[fg=colour1]$icon_zoom|;s|0|$icon_active|:window_zoomed_flag}#[fg=colour3] #W"
-tmux setw -g window-status-format "#[fg=colour8] $icon_inactive #W"
+tmux setw -g window-status-format "#[fg=$colour_fg] $icon_inactive #W"
 tmux set -g status-left '#[bg=colour236,fg=colour6]#{?#{==:#S,0},,  #S}  #[default] '
 # ^ #{?#{==:#S,0}} = if the current session is 0, don't show the session number
 tmux set -g status-right ' '
